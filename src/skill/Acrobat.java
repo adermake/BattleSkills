5ffd6751b066633afeaa60da76006cab1595fb12
Lea <Lea@LEA.fritz.box>
2020-11-04 15:14:04 +0100

    Acrobat Skill

---
+++ src/skill/Acrobat.java
@@ -0,0 +1,78 @@
+package skill;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.block.Block;
+import org.bukkit.event.Event;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import core.main;
+import skillcore.Skill;
+
+
+
+public class Acrobat extends Skill {
+	
+	
+	
+public Acrobat() {
+		
+	}
+
+	@Override
+	public void onSkillLoop() {	
+		
+	}
+
+	@Override
+	public void onSkillStart() {		
+		
+	}
+
+	@Override
+	public void onSkillEnd() {
+		
+	}
+
+	@Override
+	public void onSkillToggleOff() {
+		
+	}
+
+	@Override
+	public void onSkillToggleOn() {	
+		Location loc = user.getEyeLocation();
+		Block b = null;
+		for(double i=0; i<1;i+=0.1) {
+			Location ray = loc.clone().add(loc.getDirection().multiply(i));
+			if(ray.getBlock().getType().isSolid()) {
+				b = ray.getBlock();
+				break;
+			}
+		}
+		if(b!=null) {
+			user.setVelocity(user.getEyeLocation().getDirection().multiply(-1.5));
+			playSound(Sound.ENTITY_ENDER_DRAGON_FLAP, loc, 1, 2);
+		}
+		new BukkitRunnable() {
+
+			@Override
+			public void run() {
+				if(user.isOnGround()) {
+					this.cancel();
+				}
+				user.setFallDistance(-10);
+			}
+			
+		}.runTaskTimer(main.plugin, 5, 1);
+		
+		toggleSkill(false);
+	}
+
+	@Override
+	public void onEvent(Event e) {
+		
+	}
+
+}
